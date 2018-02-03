49197d4e3bae6e0c236f768ec75d1e7352be761d
nishisuke <nishisuke13+git@gmail.com>
2018-02-03 19:47:14 +0900

    Initialize gem

---
+++ lib/simple/period.rb
@@ -0,0 +1,7 @@
+require "simple/period/version"
+
+module Simple
+  module Period
+    # Your code goes here...
+  end
+end
---
+++ lib/simple/period/version.rb
@@ -0,0 +1,5 @@
+module Simple
+  module Period
+    VERSION = "0.1.0"
+  end
+end
